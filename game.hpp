ba5de2e6de50470a61cca8c0481e3b27b5b9bc62
Lisesharte-p <2435513848@qq.com>
2026-01-12 13:21:12 +0800

    game and snake class completed

--- game.hpp
+++ game.hpp
@@ -22,13 +22,19 @@ public:
 
   unsigned int num_snakes;
   snake_t *snakes;
-  char get_board_at(game_t *game, unsigned int row, unsigned int col);
+  game_t(int rows,int columns);
+  char get_board_at(unsigned int row, unsigned int col);
+  void set_board_at(unsigned int row, unsigned int col, char ch);
+  void print_board( FILE *fp);
+  void save_board( char *filename);
+  char next_square(unsigned int snum);
+  void update_tail(unsigned int snum);
+  void update_head(unsigned int snum);
+  void update_game(int (*add_food)(game_t *game));
+  void find_head(unsigned int snum);
 };
-game_t *create_default_game(int rows,int columns);
-void free_game(game_t *game);
-void print_board(game_t *game, FILE *fp);
-void save_board(game_t *game, char *filename);
-void update_game(game_t *game, int (*add_food)(game_t *game));
+
+
 
 game_t *initialize_snakes(game_t *game);
 game_t *load_board(FILE *fp);
